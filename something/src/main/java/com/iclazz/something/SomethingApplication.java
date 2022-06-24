e88b83a03a6887af6c2c4a264952e9dbb9707950
yiqunz <yiqunz@foxmail.com>
2022-06-24 16:03:07 +0800

    test

---
+++ something/src/main/java/com/iclazz/something/SomethingApplication.java
@@ -0,0 +1,13 @@
+package com.iclazz.something;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomethingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomethingApplication.class, args);
+    }
+
+}
---
+++ something/src/test/java/com/iclazz/something/SomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iclazz.something;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomethingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
